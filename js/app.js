f9940ff3454e5bb06e4d2f3e7e45b026ed357dea
Apelsin16 <apelsin@i.ua>
2018-09-18 17:43:33 +0300

    ready

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
-$(document).foundation()
+$(document).foundation();
 
-// $(document).ready(function () {
-//             $("a[rel=group3]").fancybox({
-//                 'transitionIn': 'none',
-//                 'transitionOut': 'none',
-//                 'titlePosition': 'over',
-//                 'titleFormat': function (title, currentArray, currentIndex, currentOpts) {
-//                     return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
-//                 }
-//             })
-//         });
+
+
