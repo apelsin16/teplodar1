baf8b21fdfc2e6b5284342d63c7f0b5072f520d2
Apelsin16 <apelsin@i.ua>
2018-09-18 16:49:54 +0300

    footer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation()
+
+// $(document).ready(function () {
+//             $("a[rel=group3]").fancybox({
+//                 'transitionIn': 'none',
+//                 'transitionOut': 'none',
+//                 'titlePosition': 'over',
+//                 'titleFormat': function (title, currentArray, currentIndex, currentOpts) {
+//                     return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+//                 }
+//             })
+//         });
