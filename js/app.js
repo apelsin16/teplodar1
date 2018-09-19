7e7d8b5d1211c26a74014188519a2c591626cbe8
Apelsin16 <apelsin@i.ua>
2018-09-19 10:07:12 +0300

    ready

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 $(document).foundation();
+(function(){
+    var map;
 
+    function initMap() {
+        var position = {
+                lat: 50.441809,
+                lng: 30.666678
+            };
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: position,
+            zoom: 18
+        });
+        var marker = new google.maps.Marker({position: position, map: map});
+    }
+})();
 
 
